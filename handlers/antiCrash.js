01aa6566def4ab8042b7a307fa624a465adcd44c
CASPERg267 <74456381+CASPERg267@users.noreply.github.com>
2022-11-24 06:06:48 +0200

    Done.

--- handlers/antiCrash.js
+++ handlers/antiCrash.js
@@ -1,19 +1,19 @@
 module.exports = (client) => {
 
     process.on('unhandledRejection', (reason, p) => {
-        client.logger.error(`Unhandled Rejection, (${reason} ${p})`, { label: `Anti Crash Module` })
+        client.logger.error(`Unhandled Rejection, (${JSON.stringify(reason, p)})`, { label: `Anti Crash Module` })
 
     });
     process.on("uncaughtException", (err, origin) => {
-        client.logger.error(`Uncaught Exception, (${err} ${origin})`, { label: `Anti Crash Module` })
+        client.logger.error(`Uncaught Exception, (${JSON.stringify(err, origin)})`, { label: `Anti Crash Module` })
 
     })
     process.on('uncaughtExceptionMonitor', (err, origin) => {
-        client.logger.error(`Uncaught Exception Monitor, (${err} ${origin})`, { label: `Anti Crash Module` })
+        client.logger.error(`Uncaught Exception Monitor, (${JSON.stringify(err, origin)})`, { label: `Anti Crash Module` })
 
     });
     process.on('multipleResolves', (type, promise, reason) => {
-        client.logger.error(`Multiple Resolves, (${type} ${promise} ${reason})`, { label: `Anti Crash Module` })
+        //client.logger.error(`Multiple Resolves, (${JSON.stringify(type, promise, reason)})`, { label: `Anti Crash Module` })
 
     });
 }
